9bbd4c7f64082be31df13c9444a2b43ca80f2dd7
Nikolija <nikolija.cvetkovic.fon@gmail.com>
2026-08-30 15:29:13 +0200

    final

--- monitoring.py
+++ monitoring.py
@@ -1,20 +1,9 @@
 import psutil
 
-
 cpu = psutil.cpu_percent(interval=1)
 ram = psutil.virtual_memory().percent
 disk = psutil.disk_usage("/").percent
 
-print(f"CPU={cpu}")
-print(f"RAM={ram}")
-print(f"DISK={disk}")
-
-
-
-
-
-
-
-
-
-
+print(f"CPU={cpu}%")
+print(f"RAM={ram}%")
+print(f"DISK={disk}%")
